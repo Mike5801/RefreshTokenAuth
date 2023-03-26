64cb8a9a9d3c8aab565b30ff24d10c062280108e
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-03-26 17:18:25 -0600

    add user model

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    user: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+      max: 20
+    },
+    occupation: String,
+    picture: String,
+    birthDate: Date
+  }
+)
+
+const User = mongoose.model("User", UserSchema)
+export default User
